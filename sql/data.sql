42f6f425db7c0421cf680f266b7cb8b9ca717287
Suvisp <suvi.s.partanen@gmail.com>
2020-03-22 00:02:43 +0200

    vielä kokeilu ilman hipsuja

--- sql/data.sql
+++ sql/data.sql
@@ -3,5 +3,5 @@ ALTER SEQUENCE topics_id_seq RESTART;
 
 INSERT INTO topics(title, description, timetomaster, timespent, source, startdate, inprogress, completiondate)
 VALUES
-    ('Scrum', 'Scrum master studies', '20', '20', 'scrum.org', '2020-02-01', 'FALSE', '2020-02-09')
+    ('Scrum', 'Scrum master studies', 20, 20, 'scrum.org', '2020-02-01', false, '2020-02-09')
 ;
